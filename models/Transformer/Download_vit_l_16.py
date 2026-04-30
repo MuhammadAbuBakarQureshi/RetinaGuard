2e960ff4c875e1f439d91c080e35c90311d19c5b
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-04-30 17:50:46 +0500

    create Download_vit_l_16.py

--- models/Transformer/Download_vit_l_16.py
+++ models/Transformer/Download_vit_l_16.py
@@ -1,10 +1,5 @@
-from torchvision import models
-import ml_modules
+import timm
 
-vit_l_16_weights = models.ViT_L_16_Weights.DEFAULT
-vit_l_16 = models.vit_l_16(weights=vit_l_16_weights)
-
-ml_modules.model_summary(model=vit_l_16,
-                         input_size=[32, 3, 224, 224])
-
-print(vit_l_16)
+model = timm.create_model("vit_large_patch14_dinov2",
+                  pretrained=True,
+                  num_classes=5)
